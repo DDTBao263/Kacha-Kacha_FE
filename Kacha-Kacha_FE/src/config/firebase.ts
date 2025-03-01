f166323af84825137b904622b2da6b0fda28f3ba
DDTBao263 <92386788+DDTBao263@users.noreply.github.com>
2025-03-01 21:32:15 +0700

    setup firebase auth

--- Kacha-Kacha_FE/src/config/firebase.ts
+++ Kacha-Kacha_FE/src/config/firebase.ts
@@ -1,6 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
+import { getStorage, FirebaseStorage } from 'firebase/storage';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  Auth,
+} from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,6 +25,18 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
+const storage: FirebaseStorage = getStorage(app);
+const auth: Auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export { storage };
+const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+export { storage, auth, signInWithGoogle };
