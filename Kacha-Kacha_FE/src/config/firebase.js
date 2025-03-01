d1bb78ed36740648d926de2128b15978f3633cac
DDTBao263 <92386788+DDTBao263@users.noreply.github.com>
2025-03-01 20:47:05 +0700

    Revert "Merge branch 'main' of https://github.com/DDTBao263/Kacha-Kacha_FE"
    This reverts commit 42a35a0de900154529aad7e465485b5d6a812d9d, reversing
    changes made to 5f387ed41e491aef2e3e4b8e4f0a94332af4358a.

--- Kacha-Kacha_FE/src/config/firebase.js
+++ Kacha-Kacha_FE/src/config/firebase.js
@@ -1,12 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getStorage, FirebaseStorage } from 'firebase/storage';
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  Auth,
-} from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,18 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const storage: FirebaseStorage = getStorage(app);
-const auth: Auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const storage = getStorage(app);
 
-const signInWithGoogle = () => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
-};
-
-export { storage, auth, signInWithGoogle };
+export { storage };
